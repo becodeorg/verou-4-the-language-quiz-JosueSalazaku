e36b65b951c70ffa30203495f1a3471f4638b595
Josué Salazaku <josuesalazaku@gmail.com>
2024-01-12 09:58:21 +0100

    Create an instance of the Word class

--- classes/LanguageGame.php
+++ classes/LanguageGame.php
@@ -3,6 +3,7 @@
 class LanguageGame
 {
     private array $words;
+    private player $player
 
     public function __construct()
     {
@@ -11,15 +12,20 @@ class LanguageGame
         // and are used mostly for more *static* types of data (a fixed set of translations in this case)
         foreach (Data::words() as $frenchTranslation => $englishTranslation) {
             // TODO: create instances of the Word class to be added to the words array
+        $this->word[] = new Word($frenchTranslation, $englishTranslation);
         }
+    }
+
+    public function selectRandomWord() {
+        // Option A: user visits site first time (or wants a new word)
+        // TODO: select a random word for the user to translate
     }
 
     public function run(): void
     {
         // TODO: check for option A or B
 
-        // Option A: user visits site first time (or wants a new word)
-        // TODO: select a random word for the user to translate
+
 
         // Option B: user has just submitted an answer
         // TODO: verify the answer (use the verify function in the word class) - you'll need to get the used word from the array first
