b5095e0535d4cf3ac097a857923a786a2b9ab774
Josué Salazaku <josuesalazaku@gmail.com>
2024-01-15 16:01:30 +0100

    Add reset button

--- view.php
+++ view.php
@@ -1,17 +1,31 @@
+<?php
+
+// require 'classes/LanguageGame.php';
+// require 'classes/Player.php';
+// require 'classes/Word.php';
+// require 'classes/Data.php';
+
+$game = new LanguageGame();
+
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport"
-		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Game</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Game</title>
 </head>
 <body>
-	<!-- TODO: add a form for the user to play the game -->
-	<!-- <form action="">
-		<input type="text">
-		<button></button>
-	</form> -->
+	<br>
+<form method="post">
+    <label for="nickname">Enter your nickname:</label>
+    <input type="text" id="nickname" name="nickname" required>
+    <button type="submit" name="submit">Start Game</button>
+	<button>RESET</button>
+</form>
+
 </body>
 </html>
